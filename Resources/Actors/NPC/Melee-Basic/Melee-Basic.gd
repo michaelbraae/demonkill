72609fc0bd445e0eff00959fd5adabd1b4205d91
michael <michaelbraae@gmail.com>
2020-05-09 21:28:07 +1200

    got pathfindingAI.gd working at a basic level

--- Resources/Actors/NPC/Melee-Basic/Melee-Basic.gd
+++ Resources/Actors/NPC/Melee-Basic/Melee-Basic.gd
@@ -2,7 +2,35 @@ extends "res://Resources/Scripts/NPC/PathfindingAI.gd"
 
 var attack_started = false
 var attacked = false
+var facing_direction = "down"
+var interactable = false
+var possessable = true 
+
+onready var animatedSprite = $AnimatedSprite
+
+func setInteractable(interactable_value : bool) -> void:
+	interactable = interactable_value
+
+func setPossessable(possessable_value : bool) -> void:
+	possessable = possessable_value
 
 func _on_AnimatedSprite_animation_finished() -> void:
 	attack_started = false
 	attacked = false
+
+func getAnimation() -> String:
+	if velocity.x >= 45:
+		animatedSprite.flip_h = false
+		facing_direction = "right"
+		return "run"
+	if velocity.x <= -45:
+		animatedSprite.flip_h = true
+		facing_direction = "right"
+		return "run"
+	if velocity.y != 0:
+		return "run"
+	return "idle"
+
+func _physics_process(delta):
+	._physics_process(delta)
+	animatedSprite.play(getAnimation())
--- Resources/Scripts/NPC/PathfindingAI.gd
+++ Resources/Scripts/NPC/PathfindingAI.gd
@@ -1,29 +1,21 @@
 extends KinematicBody2D
 
-onready var animatedSprite = $AnimatedSprite
+# dependencies, i know it's not clean. be nice :D
 onready var playerDetectionArea = $PlayerDetectionArea
 onready var collisionRayCast = $CollisionRayCast
-
 onready var navigation_mesh = get_parent()
 
 const PLAYER_POSITION_OFFSET = 25
 
-var interactable = false
-var possessable = false
 var path = []
 var path_ind = 0
-var move_speed = 180
+var move_speed = 100
 var move_direction = 0
 var vector_threshold = 0.1
 var player
 var velocity = Vector2()
-var facing_direction = "down"
 
-func setInteractable(interactable_value : bool) -> void:
-	interactable = interactable_value
-
-func setPossessable(possessable_value : bool) -> void:
-	possessable = possessable_value
+var path_blocked = false
 
 func setVectorThreshold(threshold) -> void:
 	vector_threshold = threshold
@@ -31,35 +23,26 @@ func setVectorThreshold(threshold) -> void:
 func setMoveSpeed(speed) -> void:
 	move_speed = speed
 
-func setPlayer(player_var) -> void:
+func setPlayer(player_var : KinematicBody2D) -> void:
 	player = player_var
-
-func getAnimation() -> String:
-	if velocity.x >= 45:
-		animatedSprite.flip_h = false
-		facing_direction = "right"
-		return "run"
-	if velocity.x <= -45:
-		animatedSprite.flip_h = true
-		facing_direction = "right"
-		return "run"
-	if velocity.y != 0:
-		return "run"
-	return "idle"
 
 func detectPlayer() -> void:
 	var playerDetectionOverlaps = playerDetectionArea.get_overlapping_areas()
 	if playerDetectionOverlaps:
 		for area in playerDetectionOverlaps:
 			if area.get_parent().get("IS_PLAYER"):
-				player = area.get_parent()
+				setPlayer(area.get_parent())
 
 func alignRayCastToPlayer() -> void:
 	collisionRayCast.rotation = get_angle_to(player.get_global_position())
 
 func detectBlockers():
 	var collider = collisionRayCast.get_collider()
-	print("collider: ", collider)
+	if collider is StaticBody2D:
+		if not path_blocked:
+			path_blocked = true
+	else:
+		path_blocked = false
 
 func setNavigationPoint(target_position : Vector2) -> void:
 	var closest_nav_point = navigation_mesh.get_closest_point(
@@ -69,7 +52,7 @@ func setNavigationPoint(target_position : Vector2) -> void:
 		global_transform.origin,
 		closest_nav_point
 	)
-	path_ind = 0
+	path_ind = 1
 
 func moveToNavigationPoint() -> void:
 	if path_ind < path.size():
@@ -100,9 +83,9 @@ func _physics_process(_delta : float) -> void:
 	if player:
 		alignRayCastToPlayer()
 		detectBlockers()
-		setPlayerLocationAsTargetVector()
-		move_and_slide(velocity)
-	elif velocity:
-		setNavigationPoint(velocity)
-		moveToNavigationPoint()
-	animatedSprite.play(getAnimation())
+		if path_blocked:
+			setNavigationPoint(player.get_global_position())
+			moveToNavigationPoint()
+		else:
+			setPlayerLocationAsTargetVector()
+			move_and_slide(velocity)
