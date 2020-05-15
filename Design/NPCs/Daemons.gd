bed0b3ac1f68f21b95d521182d9c93f8e8e22832
michael <michaelbraae@gmail.com>
2020-05-15 22:31:20 +1200

    added dashing state to BaseAI and fleshed out daemon manifest

---
+++ Design/NPCs/Daemons.gd
@@ -0,0 +1,60 @@
+# GENERIC DAEMONS
+
+# Straight from hell
+#	Fireball Imp
+#	Claws Imp   -- Imps share models
+
+
+# Knights fight together in groups.
+# They use formations and defensive strategies to make the players life difficult
+# The underlings will follow the captains "commands" and protect him 
+
+#	Daemon Knight - Sword and Shield - Defensive
+#	Daemon Knight - Mace - Reckless
+#	Daemon Knight - Archer - Aggressive Ranged
+#	Daemon Knight - Captain - Greatsword only
+
+# Projectile Weapon Daemons ? Hitscan or fast projectiles?
+#	Gunslinger Daemon - 2 Pistols
+#	Gatling Daemon
+#	Grenadier Daemon
+
+# Hybrid Daemons
+#	Chimera - Dog, Cat, Bird
+#	Daemon Spider
+
+# Magic Daemons
+#	Eyeball - Lazer beam
+#	Elemental Daemon
+#	Mesmer Daemon - Uses Illusions
+
+
+# BOSSES AND THEIR UNIQUE DAEMONS
+
+
+# Possession Boss - Rogue Lord
+#	The boss will switch between his minions as you fight him
+#		In order to beat him, you need to keep track of which enemy is possessed
+#	Rogue - Can turn translucent, does high damage with basic stab attack
+#	Dagger Throw - Teleporter - Spin to win
+
+# Gas Boss & Minions - Immune to gas attacks
+#	BOSS
+#	Wheezer (Base Level Minion, can swing and spray gas from mouth)
+#	Bloater (Heavy Minion, Shoots gas grenads from mouth and 
+#		explodes when killed or when player gets too close)
+
+# Daemon Chemist
+#	Throws a fuck load of grenades
+#	BOSS
+#	Rat mutants - Melee and pounce - Can use tunnels
+
+# Cannibal Librarian
+#	Daemon Students - Can locate secret doors in bookshelfs
+
+# Gravedigger Daemon
+#	Reanimated Corpses - Can Travel Underground?
+#	Skeletons - Shambling
+
+# Squid Lord - In the university lake
+#	Optional Boss - No minions
