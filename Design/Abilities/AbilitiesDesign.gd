246ab0136d6957c3c55325e426c5719cbc8db20c
michael <michaelbraae@gmail.com>
2020-05-20 17:25:01 +1200

    added some foundation to the possessableAI class

--- Design/Abilities/AbilitiesDesign.gd
+++ Design/Abilities/AbilitiesDesign.gd
@@ -1,16 +1,8 @@
-extends Node
+# Abilities and how they work
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# A new class -> Ability
+# Should be useable by the player and npcs
+# should be self contained
+# should have an associated animation
+# should have associated metrics, damage, range etc
+# should be instanced similar to how projectiles work
--- Resources/Scripts/NPC/PossessableAI.gd
+++ Resources/Scripts/NPC/PossessableAI.gd
@@ -8,5 +8,11 @@ var possessable = false
 func setInteractable(interactable_value : bool) -> void:
 	interactable = interactable_value
 
+func getInteractable() -> bool:
+	return interactable
+
 func setPossessable(possessable_value : bool) -> void:
 	possessable = possessable_value
+
+func getPossessable() -> bool:
+	return possessable
