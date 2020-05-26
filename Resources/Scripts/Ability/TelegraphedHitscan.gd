4a8141719676d09bba1620ce92a3d53f51617fec
michael <michaelbraae@gmail.com>
2020-05-26 23:51:54 +1200

    started fleshing out the telegraphed hitscan ability

---
+++ Resources/Scripts/Ability/TelegraphedHitscan.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+# a hit scan should go from the caster to the first collision.
+
+# a telegraph should go in a raycast from the caster to the player
+
+# after the impact, there should be a 
+#	decay effect from the caster to the collision
+
+# due to the nature of this ability. It will probably need to be a custom AI
