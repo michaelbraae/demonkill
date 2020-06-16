5d2324db9272458bf56331a0483cb9d8a1603e5d
michael <michaelbraae@gmail.com>
2020-06-17 11:53:47 +1200

    updated designs

---
+++ Design/Levels/1-Library.gd
@@ -0,0 +1,66 @@
+# LEVEL 1 - LIBRARY
+
+# Prologue:
+
+# Backstory - Not shown to player
+	# Your father and his assistant were trying to break 
+	# the curse on the vampire race
+	# for some reason (missing mcguffin?), the curse backfired on your father who became
+	# infected with an active strain
+	# your father's assistant tried to convince
+	# your father that he could beat the virus
+	# father knew that he would not survive
+	#	(but he did and he's a big fucking demon)
+	# so before he "died", he resurrected you from your tomb
+	# your father's assistant felt guilt for underestimating the disease
+	# he lies to the player about his true past.
+	# he claims he awoke you and brought you back to bring balance to the world
+	
+	# your father's assistant; a human, member of an ancient noble house
+	# that always remained loyal to their vampire overlords. 
+
+# Overview
+#	1. Prologue
+#	2. Exposure to outside world
+#	3. Possession boss -> library first -> gas boss
+#	4. gain gas power for access to second area of library and 
+#		option to return to the mansion
+#	5a. Second are of library
+#			- fight more demons
+#			- get a new weapon
+#			- fight large room encounter
+#	5b. Return to Mansion
+#			- Encounter your father finish major story quest
+#	6. Return to outside world
+#
+#	INTERIOR - DARK ANCIENT OPPULENT TOMB
+#		You awaken in a stone tomb, there is a sign of a visitor
+#		perhaps this person awoke you.
+#
+#		You rise from your tomb to find the room empty
+#		and move toward the exit
+#
+#		Upon leaving the tomb you find yourself
+#		in the main hall of the family crypt
+#
+#		The entrance to your tomb is to the side of a larger tomb
+#		This tomb clearly belongs to the most important person
+#
+#		As you move toward the exit of the crypt, holes in the floor
+#		force you to roll over the gaps
+#
+#		Some are made of a harmful dark energy
+#		to show the roll's i-frames
+#
+#		You come accross a sword in a hole in the ground
+#		as you pull the sword, an adjacent door opens
+#
+#		Through the door is a room with a few easy to deal with demons
+#		You finish them with basic attacks
+#
+#		The next room has one enemy, when you hit him he will be stunned
+#		and the game will pause.
+#
+#		The player will recieve a prompt to bite the enemy
+#		When the bite button is pressed, the game unpauses
+#		and the player gains health
---
+++ Design/Player.gd
@@ -0,0 +1,23 @@
+# Player Design
+
+# PILLARS OF COMBAT DESIGN
+	# fairly squishy forces player to play aggressive
+	# possession for unique ability use
+	#	- Possess an enemy for their abilities and effects
+	# charged hit/key move/empowered possession
+	#	- weapon, ability or possession
+	# push forward combat
+	#	- supplimented by movement, reward of aggressive play
+	#	- low health enemies are stunned
+	#	- stunned enemies can be bitten for health
+	#	- bites can be held to possess the enemy
+
+# CORE GAMEPLAY LOOP
+	# Use aggressive combat and possession to solve combat problems
+	# Move through levels and unlock weapons/abilities
+
+# gas puzzle should be in the main library
+# so once you get the ability, you can move to another part of the castle.
+# maybe that final room with a large reward.
+
+# thats level 1.. of x? $5 each, $15 for the lot?
