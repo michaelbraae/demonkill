a456031564f859f26e92d8ee58fa5138ce651ace
Michael Braae <michaelbraae@gmail.com>
2021-10-10 20:52:06 +1300

    Fixed the axe-recall breaking the dungeon

--- DungeonGenerator.gd
+++ DungeonGenerator.gd
@@ -89,6 +89,7 @@ func _input(event):
 		ready_for_player = false
 		player = PLAYER_SCENE.instance()
 		add_child(player)
+		GameState.player = player
 		player.position = start_room.position
 		play_mode = true
 
