8e085be6f2179cb493f5238c8d16f668ae5126f7
Michael Braae <michaelbraae@gmail.com>
2021-10-30 15:27:29 +1300

    Started deprecating the homebrew dungeon generator logic

--- Scenes/Main/TitleScreen/TitleScreen.gd
+++ Scenes/Main/TitleScreen/TitleScreen.gd
@@ -6,5 +6,5 @@ func _ready():
 	GameState.state = GameState.MAIN_MENU
 
 func _on_new_Game_button_pressed():
-#	LevelManager.goto_scene('res://Scenes/Levels/Town.tscn')
-	LevelManager.goto_scene('res://Scenes/Levels/Grid/GridBuilder.tscn')
+	LevelManager.goto_scene('res://Scenes/Levels/Town.tscn')
+#	LevelManager.goto_scene('res://Scenes/Levels/Grid/GridBuilder.tscn')
