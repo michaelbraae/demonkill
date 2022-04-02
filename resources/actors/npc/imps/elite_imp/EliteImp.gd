720dd2e47d911458dffa58154ff3e0e5393571c9
Michael Braae <michaelbraae@gmail.com>
2022-04-02 21:04:45 +1300

    elite imp button in dev tool works, elite imp works

---
+++ resources/actors/npc/imps/elite_imp/EliteImp.gd
@@ -0,0 +1,45 @@
+extends PossessableAI
+
+var SWIPE_SCENE = preload('res://resources/abilities/swipe/Swipe.tscn')
+var ABILITY_SCENE = preload('res://resources/abilities/pickup_ability/flame_swipe/FlameSwipe.tscn')
+var PICKUP_ABILITY_SCENE = preload('res://resources/abilities/pickup_ability/flame_swipe/pickup/PickupFlameSwipe.tscn')
+
+func initialiseConfig():
+	max_health = 5
+	move_speed = 70
+	attacks_in_sequence = 1
+	repeat_attacks = true
+	attack_range = 30
+	attack_cooldown = 1
+	ability_range = 50
+	ability_cooldown = 3
+	complete_attack_sequence = true
+
+func setHealth() -> void:
+	$HealthBar.max_value = max_health
+	$HealthBar.value = health
+
+func beforeDeath() -> void:
+	.beforeDeath()
+	var pickup_ability = PICKUP_ABILITY_SCENE.instance()
+	pickup_ability.position = position
+	get_tree().get_root().add_child(pickup_ability)
+
+func useAbility() -> void:
+	var ability_instance = ABILITY_SCENE.instance()
+	add_child(ability_instance)
+	if not isPossessed():
+		ability_instance.target_actor = target_actor
+	var angle = getAttackAngle()
+	ability_instance.bang(Vector2(cos(angle), sin(angle)), self)
+
+func perAttackAction() -> void:
+	var swipe_instance = SWIPE_SCENE.instance()
+	add_child(swipe_instance)
+	var angle = getAttackAngle()
+	swipe_instance.bang(Vector2(cos(angle), sin(angle)), self)
+
+func _process(_delta):
+		# need to make the AI dodge back when it enters the light and reduce dodge cooldown
+		# adding like 30 imp-melees could be really cool, like plague tale rat vibes
+	setHealth()
--- scenes/developer_tool/DeveloperTool.gd
+++ scenes/developer_tool/DeveloperTool.gd
@@ -7,7 +7,8 @@ func _ready():
 	$PlayButton.connect("button_down", self, "play_button_down")
 
 func play_button_down() -> void:
-	NPCS.push_back($NPCButtons/FireballImpButton.getSceneString())
-	NPCS.push_back($NPCButtons/FlameSwipeImpButton.getSceneString())
+	for npc_button in $NPCButtons.get_children():
+		NPCS.push_back(npc_button.getSceneString())
+#	NPCS.push_back($NPCButtons/FlameSwipeImpButton.getSceneString())
 	GameState.developer_tool_state = NPCS
 	LevelManager.goto_scene("res://scenes/developer_tool/DevRoom.tscn")
