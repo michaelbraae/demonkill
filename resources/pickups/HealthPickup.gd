1dc70be1ba297f9e2f05bcfa0ac39e6dae0f2ec4
Michael Braae <michaelbraae@gmail.com>
2022-05-02 19:32:06 +1200

    Increased the size of the detection area and fixed some of the bugs that come with that

--- resources/pickups/HealthPickup.gd
+++ resources/pickups/HealthPickup.gd
@@ -6,11 +6,13 @@ func _physics_process(_delta):
 	var overlapping_areas = $Area2D.get_overlapping_areas()
 	if overlapping_areas:
 		for area in overlapping_areas:
-			if (
-				area.get_parent() == PossessionState.getCurrentPossession()
-				&& PossessionState.getCurrentPossession() != GameState.player
-			):
-				PossessionState.getCurrentPossession().addHealth(health_amount)
-			else:
-				PlayerState.addHealth(health_amount)
-			queue_free()
+			print(area.get_name())
+			if area.get_name() == "HitBox":
+				if (
+					area.get_parent() == PossessionState.getCurrentPossession()
+					&& PossessionState.getCurrentPossession() != GameState.player
+				):
+					PossessionState.getCurrentPossession().addHealth(health_amount)
+				if area.get_parent() == GameState.player:
+					PlayerState.addHealth(health_amount)
+				queue_free()
