e0d38125fd5b354a1bcfdf2bf8d5b13e7b071dc6
Michael Braae <michaelbraae@gmail.com>
2022-05-02 12:31:08 +1200

    Fixed a bug where using the possession would break the health pickups

--- resources/pickups/HealthPickup.gd
+++ resources/pickups/HealthPickup.gd
@@ -6,6 +6,11 @@ func _physics_process(_delta):
 	var overlapping_areas = $Area2D.get_overlapping_areas()
 	if overlapping_areas:
 		for area in overlapping_areas:
-			if area.get_parent() == PossessionState.getCurrentPossession():
+			if (
+				area.get_parent() == PossessionState.getCurrentPossession()
+				&& PossessionState.getCurrentPossession() != GameState.player
+			):
+				PossessionState.getCurrentPossession().addHealth(health_amount)
+			else:
 				PlayerState.addHealth(health_amount)
-				queue_free()
+			queue_free()
