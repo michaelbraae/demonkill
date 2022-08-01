79bcdf5737193084c4a88c8e897d2be6c68f64b7
Michael Braae <michaelbraae@gmail.com>
2022-08-01 17:33:12 +1200

    Freeze doesnt stack, if a freeze is active, the freeze is not applied

--- scenes/effect/effect_handler/FreezeHandler.gd
+++ scenes/effect/effect_handler/FreezeHandler.gd
@@ -1,8 +1,10 @@
 extends DOTEffectHandler
 
 func begin_effect(effect: DotEffect) -> void:
-	.begin_effect(effect)
-	get_parent().owner.freeze(effect.effect_duration)
-	
+	if stacks < 1:
+		stacks += 1
+		effect_duration_timer.start(effect.effect_duration)
+		get_parent().owner.freeze(effect.effect_duration)
+		emit_signal(started_signal)
 	# at this point we play the existing DOT logic
 	# then we need to find a way to freeze the player
