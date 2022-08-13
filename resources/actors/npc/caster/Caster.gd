01de6adb21fd8a5d8bd9d4f41c98bdc16afaae91
Michael Braae <michaelbraae@gmail.com>
2022-08-13 14:07:04 +1200

    Fully deprecated the resources directory

--- resources/actors/npc/caster/Caster.gd
+++
@@ -1,9 +0,0 @@
-extends CasterAI
-
-func initialiseConfig() -> void:
-	.initialiseConfig()
-	attack_range = 350
-	move_spped = 180
-
-func setProjectileScene() -> void:
-	PROJECTILE_SCENE = preload('res://resources/projectiles/energy_ball/energy_ball.tscn')
