635ff545660cc947c3a3fe162d14b6075e8b11da
Michael Braae <michaelbraae@gmail.com>
2022-08-28 13:56:57 +1200

    moved some old code

---
+++ scenes/legacy/AbilityHandler/PlayerAbilityHelper.gd
@@ -0,0 +1,17 @@
+extends Node
+
+# var BOLT_SCENE = preload('res://resources/abilities/projectiles/bolt/bolt.tscn')
+
+var ability_cool_down_timer
+var ABILITY_COOL_DOWN = 2.0
+
+# get current weapon from state
+
+func fireProjectile(global_position : Vector2, attack_direction : Vector2) -> void:
+	if ability_cool_down_timer.is_stopped():
+		ability_cool_down_timer.start(ABILITY_COOL_DOWN)
+		# var bolt_instance = BOLT_SCENE.instance()
+		# get_parent().add_child(bolt_instance)
+		# bolt_instance.target_id = 'IS_ENEMY'
+		# bolt_instance.set_global_position(global_position)
+		# bolt_instance.target_direction = attack_direction
--- scenes/legacy/pump_shotgun/PumpShotgun.gd
+++ scenes/legacy/pump_shotgun/PumpShotgun.gd
@@ -1,6 +1,6 @@
 extends RotatingWeapon
 
-var ENERGY_BALL_SCENE = preload('res://resources/abilities/projectiles/energy_ball/energy_ball.tscn')
+# var ENERGY_BALL_SCENE = preload('res://resources/abilities/projectiles/energy_ball/energy_ball.tscn')
 
 const SPREAD = 0.2
 
@@ -19,11 +19,12 @@ func fire(attack_direction : Vector2) -> void:
 
 func instantiateProjectiles(attack_direction : Vector2) -> void:
 	for random_trajectory in getRandomTrajectories(attack_direction):
-		var bullet_instance = ENERGY_BALL_SCENE.instance()
-		get_parent().add_child(bullet_instance)
-		bullet_instance.target_id = 'IS_ENEMY'
-		bullet_instance.set_position(muzzle.get_position())
-		bullet_instance.target_direction = random_trajectory
+		pass
+		# var bullet_instance = ENERGY_BALL_SCENE.instance()
+		# get_parent().add_child(bullet_instance)
+		# bullet_instance.target_id = 'IS_ENEMY'
+		# bullet_instance.set_position(muzzle.get_position())
+		# bullet_instance.target_direction = random_trajectory
 
 func getRandomTrajectories(attack_direction : Vector2) -> Array:
 	var trajectories = []
