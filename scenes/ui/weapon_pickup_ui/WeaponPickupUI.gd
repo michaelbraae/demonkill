25539883440a4ff6997f90902b57fe7fbf634d43
michael <michaelbraae@gmail.com>
2022-09-10 15:11:17 +1200

    WeaponPickupUI works

--- scenes/ui/weapon_pickup_ui/WeaponPickupUI.gd
+++ scenes/ui/weapon_pickup_ui/WeaponPickupUI.gd
@@ -12,6 +12,8 @@ var weapon_to_pickup: PackedScene
 
 var selected_slot: int = 0
 
+var t = 0
+
 func initialise_weapon_pickups() -> void:
 	card_slot_new.clear_params()
 	card_slot_1.clear_params()
@@ -27,10 +29,29 @@ func swap_selected_weapon() -> void:
 		GameState.player.change_weapon_in_slot(weapon_to_pickup, selected_slot)
 		visible = false
 
+var position_target
+
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_up"):
-		card_slot_new.rect_position = top_row.position
-		selected_slot = 1
+		# card_slot_new.rect_position = top_row.position
+		if !position_target:
+			selected_slot = 1
+			t = 0
+			position_target = top_row
 	if event.is_action_pressed("ui_down"):
-		selected_slot = 2
-		card_slot_new.rect_position = bottom_row.position
+		# card_slot_new.rect_position = bottom_row.position
+		if !position_target:
+			selected_slot = 2
+			t = 0
+			position_target = bottom_row
+
+func _physics_process(delta) -> void:
+	if position_target and card_slot_new.rect_position != position_target.position:
+			var lerp_distance = lerp(card_slot_new.rect_position, position_target.position, t)
+			card_slot_new.rect_position = lerp_distance
+			
+			t += delta * 3
+
+	if position_target and card_slot_new.rect_position.distance_to(position_target.position) < 10:
+		card_slot_new.rect_position = position_target.position
+		position_target = null
