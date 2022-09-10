e4a9ab499d42101e7cddd303ca14141fe64f7205
michael <michaelbraae@gmail.com>
2022-09-10 15:46:30 +1200

    some bug fixes for the WeaponPickupUI

--- scenes/ui/weapon_pickup_ui/WeaponPickupUI.gd
+++ scenes/ui/weapon_pickup_ui/WeaponPickupUI.gd
@@ -5,8 +5,9 @@ onready var card_slot_2 = get_node("%WeaponCard2")
 onready var card_slot_new = get_node("%WeaponCardNew")
 
 # UI positions
-onready var top_row = get_node("%TopPosition")
-onready var bottom_row = get_node("%BottomPosition")
+onready var top_position = get_node("%TopPosition")
+onready var middle_position = get_node("%MiddlePosition")
+onready var bottom_position = get_node("%BottomPosition")
 
 var weapon_to_pickup: PackedScene
 
@@ -15,14 +16,19 @@ var selected_slot: int = 0
 var t = 0
 
 func initialise_weapon_pickups() -> void:
-	card_slot_new.clear_params()
-	card_slot_1.clear_params()
-	card_slot_2.clear_params()
+	reset_ui()
 	card_slot_1.weapon = PlayerState.weapon_slot_1.instance()
 	card_slot_2.weapon = PlayerState.weapon_slot_2.instance()
 	card_slot_new.set_params()
 	card_slot_1.set_params()
 	card_slot_2.set_params()
+
+func reset_ui() -> void:
+	selected_slot = 0
+	card_slot_new.rect_position = middle_position.position
+	card_slot_new.clear_params()
+	card_slot_1.clear_params()
+	card_slot_2.clear_params()
 
 func swap_selected_weapon() -> void:
 	if selected_slot and weapon_to_pickup.can_instance():
@@ -32,18 +38,17 @@ func swap_selected_weapon() -> void:
 var position_target
 
 func _unhandled_input(event):
-	if event.is_action_pressed("ui_up"):
-		# card_slot_new.rect_position = top_row.position
-		if !position_target:
-			selected_slot = 1
-			t = 0
-			position_target = top_row
-	if event.is_action_pressed("ui_down"):
-		# card_slot_new.rect_position = bottom_row.position
-		if !position_target:
-			selected_slot = 2
-			t = 0
-			position_target = bottom_row
+	if !get_tree().paused:
+		if event.is_action_pressed("ui_up"):
+			if !position_target:
+				selected_slot = 1
+				t = 0
+				position_target = top_position
+		if event.is_action_pressed("ui_down"):
+			if !position_target:
+				selected_slot = 2
+				t = 0
+				position_target = bottom_position
 
 func _physics_process(delta) -> void:
 	if position_target and card_slot_new.rect_position != position_target.position:
--- scenes/weapon/weapon_pickup/WeaponPickup.gd
+++ scenes/weapon/weapon_pickup/WeaponPickup.gd
@@ -25,17 +25,24 @@ func _ready() -> void:
 	weapon_instance.queue_free()
 
 func interacted() -> void:
-	if $WeaponCard.visible:
+	if $WeaponCard.visible and !get_tree().paused:
 		var slot_1_valid: bool = is_instance_valid(GameState.player.weapon_slot_1_instance)
 		var slot_2_valid: bool = is_instance_valid(GameState.player.weapon_slot_2_instance)
 		if slot_1_valid and slot_2_valid:
+			print('slot_1: ', GameState.player.weapon_slot_1_instance.get_name())
+			print('slot_2: ', GameState.player.weapon_slot_2_instance.get_name())
 			if !weapon_pickup_ui.visible:
+				print("!weapon_pickup_ui.visible")
 				weapon_pickup_ui.card_slot_new.weapon = weapon.instance()
 				weapon_pickup_ui.weapon_to_pickup = weapon
 				weapon_pickup_ui.initialise_weapon_pickups()
 				weapon_pickup_ui.visible = true
 			else:
+				print("weapon_pickup_ui.visible")
+				if weapon_pickup_ui.selected_slot == 0:
+					return
 				weapon_pickup_ui.swap_selected_weapon()
+				weapon_pickup_ui.visible = false
 				queue_free()
 		else:
 			if slot_1_valid:
@@ -47,7 +54,7 @@ func interacted() -> void:
 func _process(delta: float) -> void:
 	time += delta * frequency
 	icon.set_position(default_pos + Vector2(0, sin(time) * amplitude))
-	if get_global_position().distance_to(GameState.player.position) < 30:
+	if is_instance_valid(GameState.player) and get_global_position().distance_to(GameState.player.position) < 30:
 		$WeaponCard.visible = true
 	else:
 		weapon_pickup_ui.visible = false
