ab2f4ebe5d27e1c26e2eb2f2d71b275d2a21079d
michael <michaelbraae@gmail.com>
2022-09-24 15:29:52 +1200

    Added the first audio implementation

--- scenes/singletons/PossessionState.gd
+++ scenes/singletons/PossessionState.gd
@@ -20,6 +20,7 @@ func is_player(character: KinematicBody2D) -> bool:
 
 func onPossessionExit() -> void:
 	UIManager.get_node("Tsukuyomi").visible = false
+	AudioManager.stop_audio(AudioManager.HEARTBEAT)
 	# give the player back the axe if they possessed the enemy with axe
 	# could be moved to an "possessed" signal that emits when the player or an NPC is possessed
 	GameState.player.has_axe = !is_instance_valid(GameState.npc_with_axe)
@@ -80,6 +81,7 @@ func exitPossession(spawn_position) -> void:
 
 func possessEntity(new_possession) -> void:
 	UIManager.get_node("Tsukuyomi").visible = true
+	AudioManager.play_audio(AudioManager.HEARTBEAT)
 	connectToInputSignals(new_possession)
 	current_possession = new_possession
 	possessedNPC = new_possession
