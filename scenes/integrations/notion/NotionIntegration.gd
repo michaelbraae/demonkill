cfea3917fb8e07faad03e82617afa1f26a2fea97
michael <michaelbraae@gmail.com>
2022-10-08 22:05:25 +1300

    Weapons can be downloaded from notion, images can be pulled but not finished

--- scenes/integrations/notion/NotionIntegration.gd
+++ scenes/integrations/notion/NotionIntegration.gd
@@ -3,11 +3,14 @@ extends Node2D
 
 class_name NotionIntegration
 
+signal image_download_finished
+
 const API_URL: String = "https://api.notion.com/v1/databases/"
 const URL_SUFFIX: String = "/query"
 const NOTION_API_KEY: String = "secret_vDZADBQjyyhAvo72UDzPHZuYsZLBR7i5RBsfZlY6ERP"
-var DATABASE_ID: String = "17e5b08741fb4d1d95d321bb53770619"
 const NOTION_VERSION: String = "2022-02-22"
+
+var DATABASE_ID: String = "17e5b08741fb4d1d95d321bb53770619"
 var query: String = ""
 
 func _ready():
@@ -20,7 +23,6 @@ func make_request() -> void:
 		"Content-Type: application/json"
 	]
 	
-	
 	$HTTPRequest.request(
 		str(API_URL, DATABASE_ID, URL_SUFFIX),
 		headers,
@@ -31,3 +33,26 @@ func make_request() -> void:
 
 func _on_request_completed(_result, response_code, _headers, body) -> void:
 	pass
+
+func download_texture(url : String, file_name : String) -> ImageTexture:
+	file_name = file_name.replace(" ", "")
+	var http = HTTPRequest.new()
+	add_child(http)
+	
+	var icon_path = str("res://test/images/", file_name)
+	http.set_download_file(icon_path)
+	http.request(url)
+	
+	print("downloading image..")
+	
+	yield(http, "request_completed")
+	
+	var texture = ImageTexture.new()
+	var image = Image.new()
+	
+	print("download complete.. icon_path: ", icon_path)
+	
+	emit_signal("image_download_finished")
+	image.load(icon_path)
+	texture.create_from_image(image)
+	return texture
--- scenes/integrations/notion/weapons/NotionWeapons.gd
+++ scenes/integrations/notion/weapons/NotionWeapons.gd
@@ -1,13 +1,24 @@
 tool
 extends NotionIntegration
 
+var WEAPON_SCENE = preload("res://scenes/weapon/Weapon.tscn")
+var WEAPON_PICKUP_SCENE = preload("res://scenes/weapon/weapon_pickup/WeaponPickup.tscn")
+
 export(bool) var import_weapons setget fetch_weapons
+
+var weapon_created_count: int = 0
 
 func _ready() -> void:
 	DATABASE_ID = "17e5b08741fb4d1d95d321bb53770619"
 
-func fetch_weapons(_value):
-	make_request()
+func fetch_weapons(value):
+	if value:
+		var dir = Directory.new()
+		dir.open("res://test/")
+		dir.make_dir("weapons")
+		dir.make_dir("weapon_pickups")
+		dir.make_dir("images")
+		make_request()
 
 func _on_request_completed(_result, response_code, _headers, body) -> void:
 	if response_code == HTTPClient.RESPONSE_OK:
@@ -15,11 +26,56 @@ func _on_request_completed(_result, response_code, _headers, body) -> void:
 		for weapon in json.result.results:
 			if weapon.properties.name.title.size() > 0:
 				var name = weapon.properties.name.title[0]["text"].content
-				print(name)
-				var type = weapon.properties.type.select.name
-				print(type)
+				var archetype = weapon.properties.type.select.name
 				var rarity = weapon.properties.rarity.select.name
-				print(rarity)
 				var affinity = weapon.properties.affinity.select.name
-				print(affinity)
-				print("----------")
+				var icon_url = get_icon_url(weapon)
+				save_weapon(name, archetype, affinity, rarity, icon_url)
+		if weapon_created_count > 0:
+			print(str(weapon_created_count, " Weapons Successfully Created"))
+			weapon_created_count = 0
+
+func get_icon_url(weapon_json) -> String:
+	if weapon_json.properties.icon.files.size() > 0:
+		return weapon_json.properties.icon.files[0].file.url
+	return ""
+
+func save_weapon(name: String, archetype: String, affinity: String, rarity: String, icon_url: String = "") -> void:
+	var new_weapon = WEAPON_SCENE.instance()
+	new_weapon.weapon_name = name
+	new_weapon.archetype = archetype.to_upper()
+	new_weapon.rarity = rarity.to_upper()
+	new_weapon.affinity = affinity.to_upper()
+
+#	if icon_url != "":
+#		new_weapon.weapon_icon = download_texture(icon_url, str(name, ".png"))
+#		var icon_texture = download_texture(icon_url, str(name, ".png"))
+#		yield(self, "image_download_finished")
+#		new_weapon.weapon_icon = icon_texture
+
+	var scene = PackedScene.new()
+	var scene_name = str(name.replace(" ", ""), ".tscn")
+	
+	var result = scene.pack(new_weapon)
+	if result == OK:
+		var save_result = ResourceSaver.save(str("res://test/weapons/", scene_name), scene)
+		if save_result == OK:
+			weapon_created_count += 1
+			save_weapon_pickup(new_weapon)
+		else:
+			push_error(str("An error has occured while attempting to save weapon to disk. Weapon Name: ", name))
+
+func save_weapon_pickup(weapon: Weapon) -> void:
+	if is_instance_valid(weapon) and weapon.weapon_name:
+		var pickup_scene = WEAPON_PICKUP_SCENE.instance()
+		pickup_scene.weapon = weapon
+		
+		var scene = PackedScene.new()
+		var scene_name = str(weapon.weapon_name.replace(" ", ""), "Pickup.tscn")
+		
+		var result = scene.pack(pickup_scene)
+		if result == OK:
+			var save_result = ResourceSaver.save(str("res://test/weapon_pickups/", scene_name), scene)
+			if save_result != OK:
+				push_error(str("An error has occured while attempting to save weapon to disk. Weapon Name: ", weapon.weapon_name))
+			
